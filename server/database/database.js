5c5543dd2403a1e7acb574cfa8a19e0817d4edb2
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-04-03 02:23:59 +0800

    ver 1

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var yourDB = "postgres://kpjzummocwbjzi:596e3ee0d484ae7b669c48fc7b425d252e7292e3d5ca6b8b0c04a10ac9d56b9b@ec2-107-20-141-145.compute-1.amazonaws.com:5432/dbhk4v7jk8oosd";
+var yourDB = "postgres://vjggpocwokyfdd:2d64c1698c99e878bab4d25bbf4cd57d67419cafb4bab67e0d29314b750601cc@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d9sr28fl80k5dk";
 var sequelize = new Sequelize(yourDB, {
     dialect: 'postgres',
     dialectOptions: {
